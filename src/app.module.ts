a61985bd1e2306c61f2c6b6da43052d915353a40
Ndubuisi Ollawa <ndollawa@yahoo.com>
2024-08-24 13:54:38 +0100

    task completed

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,15 @@ import { ThrottlerModule } from '@nestjs/throttler';
 import { PostModule } from './post/post.module';
 import { CommentModule } from './comment/comment.module';
 import { PrismaModule } from '@app/prisma';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development.local', '.env.development', '.env'],
+      isGlobal: true,
+      cache: true,
+    }),
     AutobotModule,
     ThrottlerModule.forRoot([{ ttl: 60, limit: 5 }]),
     PostModule,
